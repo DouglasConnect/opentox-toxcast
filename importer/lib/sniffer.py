29459cdff0f6945d9aea678986c6518a7946a9bb
Joh Dokler <joh.dokler@gmail.com>
2016-11-03 08:35:43 +0100

    Skeleton

---
+++ importer/lib/sniffer.py
@@ -0,0 +1,15 @@
+class Sniffer(object):
+
+    def __init__(header, aggregation_term_limit=10):
+        pass
+
+    def add_row():
+        pass
+
+
+class Column(object):
+
+    def __init__(name, aggregation_term_limit=10):
+        name = name
+        values = set()
+        
